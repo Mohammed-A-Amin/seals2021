874f81b991e1e5abf7cb977a4d85ce58b226ec46
Mohammed Amin <coldprohor12@gmail.com>
2021-02-19 20:21:34 -0800

    Angular stuff

--- angular-projects/mathapp/src/app/app-routing.module.ts
+++ angular-projects/mathapp/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { TipsComponent } from './tips/tips.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent},
+  { path: 'tips', component: TipsComponent},
+  { path: 'schedule', component: ScheduleComponent},
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-projects/mathapp/src/app/app.module.ts
+++ angular-projects/mathapp/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { TipsComponent } from './tips/tips.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    HomeComponent,
+    TipsComponent,
+    ScheduleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
